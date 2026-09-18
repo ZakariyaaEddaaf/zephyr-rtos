6480239f452d2c83e08b0c6bac9021a217549953
zakariyaa.eddaaf <zakariyaa.eddaaf@evologics.de>
2026-09-18 23:25:05 +0200

    static (build-time) threads

--- applications/little_brain/src/main.c
+++ applications/little_brain/src/main.c
@@ -3,66 +3,69 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#include "led_utils.h"
-#include "switch_utils.h"
 #include <stdio.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/sys/printk.h>
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS 500
+#define MY_STACK_SIZE 1024
+#define PRIORITY 5
+
+#define LED0_SLEEP_MS 500
+#define LED1_SLEEP_MS 1000
 
 #define LED0_NODE DT_ALIAS(led0)
 #define LED1_NODE DT_ALIAS(led1)
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
-static const struct gpio_dt_spec sw0 = GPIO_DT_SPEC_GET(DT_ALIAS(sw0), gpios);
 
-static void board_init();
-static void board_run();
+static void toggle_led0(void *p1, void *p2, void *p3) {
+    ARG_UNUSED(p1);
+    ARG_UNUSED(p2);
+    ARG_UNUSED(p3);
 
-void board_init() {
-    // setup led0 and led1
-    led_setup(&led0);
-    led_setup(&led1);
-    // setup switch0
-    switch_setup(&sw0);
-}
-
-void board_run() {
-
-    static bool leds_active = false;
-    static bool was_pressed = false;
-
-    bool pressed = switch_state(&sw0);
-
-    if (was_pressed && !pressed) {
-        leds_active = !leds_active;
-        if (!leds_active) {
-            led_off(&led0);
-            led_off(&led1);
-        }
+    if (!gpio_is_ready_dt(&led0)) {
+        LOG_ERR("led0 GPIO port not ready");
+        return;
     }
-    was_pressed = pressed;
 
-    if (leds_active) {
-        led_toggle(&led0);
-        led_toggle(&led1);
-        k_msleep(500);
-    } else {
-        k_msleep(20);
+    if (gpio_pin_configure_dt(&led0, GPIO_OUTPUT_INACTIVE) < 0) {
+        LOG_ERR("failed to configure led0");
+        return;
     }
-}
 
-int main(void) {
-    board_init();
     while (1) {
-        board_run();
+        gpio_pin_toggle_dt(&led0);
+        k_msleep(LED0_SLEEP_MS);
     }
-    return 0;
 }
+
+static void toggle_led1(void *p1, void *p2, void *p3) {
+    ARG_UNUSED(p1);
+    ARG_UNUSED(p2);
+    ARG_UNUSED(p3);
+
+    if (!gpio_is_ready_dt(&led1)) {
+        LOG_ERR("led1 GPIO port not ready");
+        return;
+    }
+
+    if (gpio_pin_configure_dt(&led1, GPIO_OUTPUT_INACTIVE) < 0) {
+        LOG_ERR("failed to configure led1");
+        return;
+    }
+
+    while (1) {
+        gpio_pin_toggle_dt(&led1);
+        k_msleep(LED1_SLEEP_MS);
+    }
+}
+
+K_THREAD_DEFINE(toggle_led0_tid, MY_STACK_SIZE, toggle_led0, NULL, NULL, NULL,
+                PRIORITY, 0, 0);
+
+K_THREAD_DEFINE(toggle_led1_tid, MY_STACK_SIZE, toggle_led1, NULL, NULL, NULL,
+                PRIORITY, 0, 0);
